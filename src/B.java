084fc8838f8d295310d7c5905b02a215520593d4
Constructor <dilshanjayathilaka8114@gmail.com>
2023-04-26 18:05:50 +0530

    Condition Codes

---
+++ src/B.java
@@ -0,0 +1,13 @@
+public class B {
+
+    public static void main(String[] args) {
+        String a = "Dilshan";
+        if(a=="dilshan"){
+            System.out.println("dilshan");
+        } else if (a == "Dilshan") {
+            System.out.println("Dilshan");
+        }else {
+            System.out.println("False");
+        }
+    }
+}
